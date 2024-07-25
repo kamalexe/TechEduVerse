8fdb9f3001864bfdc57ef2f9a3e45f64aa95111b
Kamal Maurya <kamal38005@gmail.com>
2024-07-26 00:01:24 +0530

    remove grappelli

--- TechEduVerse/settings.py
+++ TechEduVerse/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'widget_tweaks',
-    'grappelli',
+    # 'grappelli',
     'courses', 
     'django.contrib.admin',
     'django.contrib.auth',
--- TechEduVerse/urls.py
+++ TechEduVerse/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),  # grappelli URLS
+    # path('grappelli/', include('grappelli.urls')),  # grappelli URLS
     path('admin/', admin.site.urls),
     path('', include('courses.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),  # Include auth URLs
